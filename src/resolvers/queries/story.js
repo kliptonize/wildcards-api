0b4c516be422aa3bc10447c85385f3678384da0c
Koen <koen@beformed.be>
2017-08-09 21:56:58 +0200

    Update ‘last_seen’ of followed story on query
    - Update ‘last_seen’ value of a followed story when story is query individually

--- src/resolvers/queries/story.js
+++ src/resolvers/queries/story.js
@@ -1,7 +1,19 @@
 //Returns only ONE story based on ID
 const {ObjectId} = require('mongodb');
 
-module.exports = async (root,  {id}, {mongo: {Stories, Cards}, device}) => {
+module.exports = async (root,  {id}, {mongo: {Stories, Cards, Follows}, device}) => {
+	//Check if device follows this story, and update the last_seen variable
+	var follow = await Follows.findOne({device_id: device._id, story_id: id});
+	if(follow){
+		Follows.update({
+			_id: follow._id
+		}, {
+			$set: {
+				last_seen: Math.floor(Date.now() / 1000),
+				_modified: Math.floor(Date.now() / 1000)
+			}
+		});
+	}
 	var story = await Stories.findOne({_id: new ObjectId(id)});
 	if(story == null){
 		throw new Error(`Story (${id}) not found`);
