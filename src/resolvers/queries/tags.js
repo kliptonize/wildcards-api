6f49edaf25fe9fb3e8b86174100a399025e0c0de
Koen <koen@beformed.be>
2017-11-15 21:23:02 +0100

    Fixed several issues
    - call “topics”
    - call “tags”
    work, but not sorted yet

--- src/resolvers/queries/tags.js
+++ src/resolvers/queries/tags.js
@@ -1,19 +1,19 @@
 //Returns all tags
 
-module.exports = async (root, data, {mongo: {Sets}, device}) => {
+module.exports = async (root, data, {mongo: {Stories}, device}) => {
 	//Perform a distinct query for all tags
-	var distinctTags = await Sets.distinct('tags');
+	var distinctTags = await Stories.distinct('tags');
 
 	//Loop over array, creates objects
 	var returnArray = [];
-	distinctTags.forEach(function(tag){
+	distinctTags.forEach(async function(tag){
 		returnArray.push({
 			"name": "#" + tag
 		});
-	})
+	});
 
 	return returnArray;
 
 	//TODO: sort them on most popular in the last x -days
-	//Sets.count({tags: {$in: [tag]}});
+	//Stories.count({tags: {$in: [tag]}});
 };
--- src/resolvers/queries/topics.js
+++ src/resolvers/queries/topics.js
@@ -1,13 +1,13 @@
 //Returns all tags
 
-module.exports = async (root, data, {mongo: {Topics, Sets}, device}) => {
+module.exports = async (root, data, {mongo: {Topics, Stories}, device}) => {
 	//Perform a distinct query for all tags
 	var topics = await Topics.find().toArray();
 
 	//Loop over array, creates objects
 	for(var i = 0, len = topics.length; i < len; i++){
-		var count = await Sets.count({"topic._id": topics[i]._id});
-		topics[i].amountOfSets = count;
+		var count = await Stories.count({"topic._id": topics[i]._id});
+		topics[i].amountOfStories = count;
 	}
 
 	return topics;
