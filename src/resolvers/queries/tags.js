da1c3908401efa00db1517b1a9f99842e010ff43
Koen <koen@beformed.be>
2017-11-15 22:58:25 +0100

    Added amount of stories to tags

--- src/resolvers/queries/tags.js
+++ src/resolvers/queries/tags.js
@@ -6,13 +6,22 @@ module.exports = async (root, data, {mongo: {Stories}, device}) => {
 
 	//Loop over array, creates objects
 	var returnArray = [];
-	distinctTags.forEach(async function(tag){
-		returnArray.push({
-			"name": "#" + tag
-		});
-	});
 
-	return returnArray;
+	//Loop over array, creates objects
+	for(var i = 0, len = distinctTags.length; i < len; i++){
+		tag = distinctTags[i];
+		var amount = await Stories.count({tags: {$in: [tag]}});
+		returnArray.push({
+			"name": "#" + tag,
+			"amount": amount
+		});
+	}
+
+	//Sort by count
+    return returnArray.sort(function(a, b) {
+        var x = a["amount"]; var y = b["amount"];
+        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+    });
 
 	//TODO: sort them on most popular in the last x -days
 	//Stories.count({tags: {$in: [tag]}});
--- src/schema/types/tag.js
+++ src/schema/types/tag.js
@@ -1,6 +1,7 @@
 const Tag = `
 	type Tag {
 		name: String!
+		amount: Int
 }`;
 
 module.exports = Tag;
